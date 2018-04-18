fc01f81f296b989643c5b4a62421336e361fe7c0
Krzysztof Socha <k.socha@merixstudio.com>
2018-04-18 03:01:58 +0200

    Make url imports less verbose

--- djangocms_alias/urls.py
+++ djangocms_alias/urls.py
@@ -1,66 +1,47 @@
 from django.conf.urls import url
 
-from .constants import (
-    CATEGORY_LIST_URL_NAME,
-    CREATE_ALIAS_URL_NAME,
-    DELETE_ALIAS_PLUGIN_URL_NAME,
-    DETACH_ALIAS_PLUGIN_URL_NAME,
-    DETAIL_ALIAS_URL_NAME,
-    LIST_ALIASES_URL_NAME,
-    PUBLISH_ALIAS_URL_NAME,
-    SET_ALIAS_DRAFT_URL_NAME,
-)
-from .views import (
-    AliasDeleteView,
-    AliasDetailView,
-    AliasListView,
-    CategoryListView,
-    create_alias_view,
-    detach_alias_plugin_view,
-    publish_alias_view,
-    set_alias_draft_mode_view,
-)
+from . import constants, views
 
 
 urlpatterns = [
     url(
         r'^create-alias/$',
-        create_alias_view,
-        name=CREATE_ALIAS_URL_NAME,
+        views.create_alias_view,
+        name=constants.CREATE_ALIAS_URL_NAME,
     ),
     url(
         r'^publish-alias/(?P<pk>\d+)/(?P<language>\w+)/$',
-        publish_alias_view,
-        name=PUBLISH_ALIAS_URL_NAME,
+        views.publish_alias_view,
+        name=constants.PUBLISH_ALIAS_URL_NAME,
     ),
     url(
         r'^aliases/$',
-        CategoryListView.as_view(),
-        name=CATEGORY_LIST_URL_NAME,
+        views.CategoryListView.as_view(),
+        name=constants.CATEGORY_LIST_URL_NAME,
     ),
     url(
         r'^aliases/category/(?P<category_pk>\d+)/$',
-        AliasListView.as_view(),
-        name=LIST_ALIASES_URL_NAME,
+        views.AliasListView.as_view(),
+        name=constants.LIST_ALIASES_URL_NAME,
     ),
     url(
         r'^aliases/(?P<pk>\d+)/$',
-        AliasDetailView.as_view(),
-        name=DETAIL_ALIAS_URL_NAME,
+        views.AliasDetailView.as_view(),
+        name=constants.DETAIL_ALIAS_URL_NAME,
     ),
     url(
         r'^detach-alias/$',
-        detach_alias_plugin_view,
-        name=DETACH_ALIAS_PLUGIN_URL_NAME,
+        views.detach_alias_plugin_view,
+        name=constants.DETACH_ALIAS_PLUGIN_URL_NAME,
     ),
     url(
         r'^delete-alias/(?P<pk>\d+)/$',
-        AliasDeleteView.as_view(),
-        name=DELETE_ALIAS_PLUGIN_URL_NAME,
+        views.AliasDeleteView.as_view(),
+        name=constants.DELETE_ALIAS_PLUGIN_URL_NAME,
     ),
     url(
         r'^draft-aliases-mode/$',
-        set_alias_draft_mode_view,
-        name=SET_ALIAS_DRAFT_URL_NAME,
+        views.set_alias_draft_mode_view,
+        name=constants.SET_ALIAS_DRAFT_URL_NAME,
     ),
 ]

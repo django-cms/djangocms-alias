a9a20cd5552fef016b7aeede26d5c5c30d1f55eb
Krzysztof Socha <k.socha@merixstudio.com>
2018-04-25 17:39:03 +0200

    JS formatting

--- djangocms_alias/static/djangocms_alias/js/create_alias_plugin.js
+++ djangocms_alias/static/djangocms_alias/js/create_alias_plugin.js
@@ -1,45 +1,47 @@
 $(function() {
-  var itemsPerPage = 30;
-  var categoryField = $('#id_category');
-  var aliasField = $('#id_alias');
-  var aliasEndpoint = aliasField.attr('data-select2-url');
-  categoryField.select2({
-    allowClear: true,
-  });
-  aliasField.select2({
-    ajax: {
-        url: aliasEndpoint,
-        dataType: 'json',
-        quietMillis: 250,
-        data: function (term, page) {
-          return {
-            term: term,
-            page: page,
-            limit: itemsPerPage,
-            category: categoryField.val(),
-          }
+    var itemsPerPage = 30;
+    var categoryField = $('#id_category');
+    var aliasField = $('#id_alias');
+    var aliasEndpoint = aliasField.attr('data-select2-url');
+    categoryField.select2({
+        allowClear: true
+    });
+    aliasField.select2({
+        ajax: {
+            url: aliasEndpoint,
+            dataType: 'json',
+            quietMillis: 250,
+            data: function(term, page) {
+                return {
+                    term: term,
+                    page: page,
+                    limit: itemsPerPage,
+                    category: categoryField.val()
+                };
+            },
+            results: function(data, page) {
+                return data;
+            }
         },
-        results: function (data, page) {
-            return data;
+        initSelection: function(element, callback) {
+            var aliasId = element.val();
+            $.ajax({
+                url: aliasEndpoint,
+                dataType: 'json',
+                data: {
+                    pk: aliasId
+                }
+            })
+                .done(function(data) {
+                    var text = aliasId;
+                    if (data.results.length) {
+                        text = data.results[0].text;
+                    }
+                    callback({ id: aliasId, text: text });
+                })
+                .fail(function() {
+                    callback({ id: aliasId, text: aliasId });
+                });
         }
-    },
-    initSelection: function(element, callback) {
-      var aliasId = element.val();
-      $.ajax({
-        url: aliasEndpoint,
-        dataType: 'json',
-        data: {
-          pk: aliasId,
-        }
-      }).done(function(data) {
-        var text = aliasId;
-        if (data.results.length) {
-          text = data.results[0].text;
-        }
-        callback({id: aliasId, text: text});
-      }).fail(function() {
-        callback({id: aliasId, text: aliasId});
-      });
-    },
-  });
+    });
 });

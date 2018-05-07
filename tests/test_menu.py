85da7e9e26cbaeaf781830dc13844ba1eb829a8c
Krzysztof Socha <k.socha@merixstudio.com>
2018-05-07 14:05:32 +0200

    Fix menu modifier tests

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -7,3 +7,12 @@ class AliasMenuTestCase(BaseAliasPluginTestCase):
         with self.login_user_context(self.superuser):
             response = self.client.get(self.CATEGORY_LIST_ENDPOINT)
         self.assertInHTML('<ul class="nav"></ul>', response.content.decode())
+
+    def test_pages_keep_their_menu_nodes(self):
+        with self.login_user_context(self.superuser):
+            response = self.client.get(self.page.get_absolute_url())
+
+        self.assertInHTML(
+            '<ul class="nav"><li class="child selected"><a href="/en/test/">test</a></li></ul>',  # noqa: E501
+            response.content.decode(),
+        )

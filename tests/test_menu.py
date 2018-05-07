8827e24cf530ce52c6b8449ee255df291909ef33
Krzysztof Socha <k.socha@merixstudio.com>
2018-05-07 14:15:26 +0200

    Add a docstring to one of the tests

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -9,8 +9,10 @@ class AliasMenuTestCase(BaseAliasPluginTestCase):
         self.assertInHTML('<ul class="nav"></ul>', response.content.decode())
 
     def test_pages_keep_their_menu_nodes(self):
-        with self.login_user_context(self.superuser):
-            response = self.client.get(self.page.get_absolute_url())
+        """Tests that AliasDisableMenu modifier does not affect
+        non-alias pages"""
+
+        response = self.client.get(self.page.get_absolute_url())
 
         self.assertInHTML(
             '<ul class="nav"><li class="child selected"><a href="/en/test/">test</a></li></ul>',  # noqa: E501

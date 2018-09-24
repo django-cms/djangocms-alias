f3b23e122b41263c451e6674166b675dba9ffb37
Rizwan <rizwan@webbyfox.com>
2018-09-24 16:16:40 +0100

    Refactor

--- djangocms_alias/cms_config.py
+++ djangocms_alias/cms_config.py
@@ -2,16 +2,19 @@ from cms.app_base import CMSAppConfig
 
 from .models import AliasContent
 from .rendering import render_alias_content
-from .internal_search import AliasContentConfig
+try:
+    from .internal_search import AliasContentConfig
+except ImportError:
+    AliasContentConfig = None
 
 class AliasCMSConfig(CMSAppConfig):
     cms_enabled = True
     cms_toolbar_enabled_models = [(AliasContent, render_alias_content)]
 
-    # internalsearch config
-    djangocms_internalsearch_enabled = True
-
+    # Internalsearch configuration
     if AliasContentConfig:
+        djangocms_internalsearch_enabled = True
+
         internalsearch_config_list = [
             AliasContentConfig
         ]

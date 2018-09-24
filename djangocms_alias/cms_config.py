46055d3b6bf97410f46a3f8c4e5b49089d9dd976
Rizwan <rizwan@webbyfox.com>
2018-09-24 16:21:36 +0100

    isort

--- djangocms_alias/cms_config.py
+++ djangocms_alias/cms_config.py
@@ -2,10 +2,13 @@ from cms.app_base import CMSAppConfig
 
 from .models import AliasContent
 from .rendering import render_alias_content
+
+
 try:
     from .internal_search import AliasContentConfig
 except ImportError:
     AliasContentConfig = None
+
 
 class AliasCMSConfig(CMSAppConfig):
     cms_enabled = True

48e24d6526d0e241cd341afb545ecbebc0299b65
Rizwan <rizwan@webbyfox.com>
2018-09-24 16:12:52 +0100

    Internal Search Integration

--- djangocms_alias/cms_config.py
+++ djangocms_alias/cms_config.py
@@ -2,8 +2,16 @@ from cms.app_base import CMSAppConfig
 
 from .models import AliasContent
 from .rendering import render_alias_content
-
+from .internal_search import AliasContentConfig
 
 class AliasCMSConfig(CMSAppConfig):
     cms_enabled = True
     cms_toolbar_enabled_models = [(AliasContent, render_alias_content)]
+
+    # internalsearch config
+    djangocms_internalsearch_enabled = True
+
+    if AliasContentConfig:
+        internalsearch_config_list = [
+            AliasContentConfig
+        ]

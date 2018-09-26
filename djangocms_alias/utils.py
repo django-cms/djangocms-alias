cc33cc4a2487b173030a245f6de9d90281512084
Mateusz Kamycki <mateusz.kamycki@gmail.com>
2018-09-26 16:43:11 +0200

    Fix

--- djangocms_alias/utils.py
+++ djangocms_alias/utils.py
@@ -4,6 +4,7 @@ from django.apps import apps
 def is_versioning_enabled():
     from .models import AliasContent
     try:
-        return apps.get_app_config('djangocms_versioning').cms_extension.is_content_model_versioned(AliasContent)
+        app_config = apps.get_app_config('djangocms_versioning')
+        return app_config.cms_extension.is_content_model_versioned(AliasContent)
     except LookupError:
         return False

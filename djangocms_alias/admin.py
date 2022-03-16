c9607a7fc73071699c796cd2ab2d3c32259e2c64
Adam Murray <adam-murray@hotmail.co.uk>
2022-03-16 11:36:33 +0000

    Feat: Added method for enclosing action methods in alias admin

--- djangocms_alias/admin.py
+++ djangocms_alias/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.contrib.contenttypes.models import ContentType
 from django.template.loader import render_to_string
 from django.urls import reverse
+from django.utils.html import format_html_join
+from django.utils.translation import gettext_lazy as _
 
 from cms.utils.permissions import get_model_permission_codename
 
@@ -91,20 +93,52 @@ class AliasAdmin(admin.ModelAdmin):
 class AliasContentAdmin(admin.ModelAdmin):
     form = AliasContentForm
     list_filter = (LanguageFilter,)
+    list_display = ["name", ]
+
+    class Media:
+        css = {
+            "all": ("djangocms_versioning/css/actions.css",)
+        }
 
     def _get_references_link(self, obj, request):
-        content_type_id = ContentType.objects.get("djangocms_alias", "aliascontent").id
+        content_type_id = ContentType.objects.get(app_label="djangocms_alias", model="aliascontent").id
 
         url = reverse(
             "djangocms_references:references-index",
             kwargs={"content_type_id": content_type_id, "object_id": obj.id},
         )
 
-        return render_to_string("djangocms_references/admin/references.html", {"url": url})
+        return render_to_string("admin/djangocms_alias/aliascontent/references.html", {"url": url})
+
+    def get_list_display(self, request):
+        # get configured list_display
+        list_display = self.list_display
+        # Add versioning information and action fields
+        list_display += [
+            self._list_actions(request)
+        ]
+        return list_display
+
+    def _list_actions(self, request):
+        """
+        A closure that makes it possible to pass request object to
+        list action button functions.
+        """
+
+        def list_actions(obj):
+            """Display links to state change endpoints
+            """
+            return format_html_join(
+                "",
+                "{}",
+                ((action(obj, request),) for action in self.get_list_actions()),
+            )
+
+        list_actions.short_description = _("actions")
+        return list_actions
 
     def get_list_actions(self):
-        return super().get_list_actions() + [self._get_references_link, ]
-
+        return [self._get_references_link, ]
 
     def save_model(self, request, obj, form, change):
         super().save_model(request, obj, form, change)

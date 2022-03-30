71d34605b24723a49e06b87d671ca371a5e1c3dc
NarenderRajuB <naren.bongoni@ELI-GU1-MAC005.local>
2022-03-30 13:13:42 +0100

    update tests

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -98,6 +98,10 @@ class AliasContentManagerTestCase(CMSTestCase):
             expected_en_content.name,
             response_content_decoded,
         )
+        self.assertNotIn(
+            expected_en_content.get_absolute_url(),
+            response_content_decoded,
+        )
 
     @skipUnless(is_versioning_enabled(), 'Test only relevant for versioning')
     def test_alias_content_manager_rendering_with_versioning_actions(self):

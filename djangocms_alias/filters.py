1659c09fe62f97cba64a2e81550e025abeffc662
Andrew Aikman <andrew.aikman@eliga.services>
2022-04-11 15:28:09 +0100

    Refine the filters

--- djangocms_alias/filters.py
+++ djangocms_alias/filters.py
@@ -39,7 +39,7 @@ from cms.forms.utils import get_sites
 class SiteFilter(admin.SimpleListFilter):
     title = _("Site")
     parameter_name = "site"
-    no_site_set_value = "none"
+    no_site_set_value = "no_site"
 
     def lookups(self, request, model_admin):
         return [(site.pk, site.name) for site in get_sites()]
@@ -49,21 +49,21 @@ class SiteFilter(admin.SimpleListFilter):
         if chosen_site and chosen_site == self.no_site_set_value:
             return queryset.filter(alias__site__isnull=True)
         elif chosen_site:
-            return queryset.filter(alias__site=chosen_site)
+            return queryset.filter(alias__site__pk=int(chosen_site))
         return queryset
 
     def choices(self, changelist):
         yield {
             "selected": self.value() is None,
             "query_string": changelist.get_query_string(remove=[self.parameter_name]),
-            "display": _("Current"),
+            "display": _("All"),
         }
         yield {
-            "selected": self.value() is self.no_site_set_value,
+            "selected": self.value() == self.no_site_set_value,
             "query_string": changelist.get_query_string(
                 {self.parameter_name: self.no_site_set_value}
             ),
-            "display": _("None"),
+            "display": _("No Site"),
         }
         for lookup, title in self.lookup_choices:
             yield {

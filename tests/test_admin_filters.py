4155342e2c997b901d2d39d2a929b66867086017
NarenderRajuB <bnraju81@gmail.com>
2022-04-25 23:31:05 +0530

    Update tests/test_admin_filters.py
    Co-authored-by: Aiky30 <Aiky30@users.noreply.github.com>

--- tests/test_admin_filters.py
+++ tests/test_admin_filters.py
@@ -178,7 +178,6 @@ class UnpublishedFiltersTestCase(BaseAliasPluginTestCase):
         from djangocms_versioning.models import Version
 
         category = Category.objects.create(name='Alias Filter Category')
-
         alias = AliasModel.objects.create(
             category=category,
             position=0

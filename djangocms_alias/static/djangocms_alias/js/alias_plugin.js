178ea7bc8424a21105754e2e60e05e7f4deb2bed
bernard <bvdv9075@gmail.com>
2022-06-09 16:32:46 +0100

    reworked default value for dropdown selections

--- djangocms_alias/static/djangocms_alias/js/alias_plugin.js
+++ djangocms_alias/static/djangocms_alias/js/alias_plugin.js
@@ -1,13 +1,13 @@
-// The current site is preselected for the user in the Site field
-// Once the site is selected with a value the Category field becomes active
-// In the Category field, the list of available categories is updated based on the selected Site
-// Once the Category field is selected, the Alias field becomes active
-// In the Alias field, the list of available aliases is updated based on the selected Category
-// If the Site field is updated, the Category field will be reset and Alias field inactive until the Category selection is made
+/* The current site is preselected for the user in the Site field
+Once the site is selected with a value the Category field becomes active
+In the Category field, the list of available categories is updated based on the selected Site
+Once the Category field is selected, the Alias field becomes active
+In the Alias field, the list of available aliases is updated based on the selected Category
+If the Site field is updated, the Category field will be reset and Alias field inactive until the Category selection
+is made
+*/
 
-const aliasDropdownFirst = "Select an alias";
-const categoryDropdownFirst = "Select category to restrict the list of aliases below";
-const siteDropDownFirst = "---------";
+const siteDropDownDefault = "---------";
 
 function disableAlias(param){
     $('#id_alias').prop( "disabled", param );
@@ -17,19 +17,17 @@ function disableCategory(param){
     $('#id_category').prop( "disabled", param );
 }
 
-// Onload we check if alias is set, if it is, we do nothing
-// if alias is not set, we check if category is set, if category is not set,
-// the alias field is disabled until populated
+/* Onload we check if alias is set, if it is, we do nothing
+if alias is not set, we check if category is set, if category is not set,
+the alias field is disabled until populated */
 $(document).ready(function() {
-    let catDiv = $('.field-category');
+    let catDiv = $(".field-category");
     if (catDiv.length){
-        let aliasSpanTag = $("#s2id_id_alias > a > span");
-        let aliasSpanText = aliasSpanTag[0].childNodes[0].textContent;
-        if (aliasSpanText == aliasDropdownFirst){
+        let aliasValue = $("#s2id_id_alias > a").hasClass("select2-default");
+        if (aliasValue == true){
             // This means alias is not set so lets check category
-            let catSpanTag = $("#s2id_id_category > a > span");
-            let catSpanText = catSpanTag[0].childNodes[0].textContent;
-            if (catSpanText == categoryDropdownFirst) {
+            let categoryValue = $("#s2id_id_category > a").hasClass("select2-default");
+            if (categoryValue == true) {
                 // Category is not set so let's disable alias
                 disableAlias(true);
             }
@@ -40,10 +38,9 @@ $(document).ready(function() {
 // Category field change event
 $(document).ready(function() {
     $('#id_category').change(function(){
-        let catSpanTag = $("#s2id_id_category > a > span")
-        let catSpanText = catSpanTag[0].childNodes[0].textContent
+        let categoryValue = $("#s2id_id_category > a").hasClass("select2-default");
         // if category is set, remove the disable attribute
-        if (catSpanText != categoryDropdownFirst) {
+        if (categoryValue == false) {
             disableAlias(false)
         }
         // In case the category field is cleared, we need to disable the alias field again
@@ -56,14 +53,15 @@ $(document).ready(function() {
 // Site field change event
 $(document).ready(function() {
     $('#id_site').change(function(){
-        // If the site changes, category should be reset
-        let catSpanTag = $("#s2id_id_category > a > span")
-        catSpanTag[0].childNodes[0].textContent = categoryDropdownFirst
+        // If the site changes, category should be reset to default
+        const categoryDropdownDefault = $('#id_category').attr("data-placeholder");
+        let catSpanTag = $("#s2id_id_category > a > span");
+        catSpanTag[0].childNodes[0].textContent = categoryDropdownDefault
         disableAlias(true)
 
         let siteName = $('#id_site').find(":selected").text();
         // If the site is changed to none, alias should be disabled
-        if(siteName == siteDropDownFirst){
+        if(siteName == siteDropDownDefault){
             disableAlias(true)
             disableCategory(true)
         }

b8276c2ec6bca917556b779848ef200a191c676a
bernard <bvdv9075@gmail.com>
2022-06-09 09:34:43 +0100

    cleanup

--- djangocms_alias/static/djangocms_alias/js/alias_plugin.js
+++ djangocms_alias/static/djangocms_alias/js/alias_plugin.js
@@ -1,3 +1,19 @@
+// The current site is preselected for the user in the Site field
+// Once the site is selected with a value the Category field becomes active
+// In the Category field, the list of available categories is updated based on the selected Site
+// Once the Category field is selected, the Alias field becomes active
+// In the Alias field, the list of available aliases is updated based on the selected Category (Existing)
+// If the Site field is updated, the Category field will be reset and Alias field inactive until the Category selection is made
+// If the user Saves without selecting an Alias, an error message will be displayed and the Plugin will not save. (Existing)
+// The Save CTA will remain active at all time. (Existing)
+
+function disableAlias(param){
+    $('#id_alias').prop( "disabled", param );
+}
+
+function disableCategory(param){
+    $('#id_category').prop( "disabled", param );
+}
 
 // Onload we check if alias is set, if it is, we do nothing
 // if alias is not set, we check if category is set, if category is not set,
@@ -13,7 +29,7 @@ $(document).ready(function() {
             let spanText = catSpanTag[0].childNodes[0].textContent
             if (spanText == "Select category to restrict the list of aliases below") {
                 // Category is not set so let's disable alias
-                $('#id_alias').prop( "disabled", true );
+                disableAlias(true)
             }
         }
     }
@@ -25,29 +41,33 @@ $(document).ready(function() {
         let catSpanTag = $("#s2id_id_category > a > span")
         let spanText = catSpanTag[0].childNodes[0].textContent
         // if category is set, remove the disable attribute
+        console.log(spanText)
         if (spanText != "Select category to restrict the list of aliases below") {
-            $('#id_alias').prop( "disabled", false );
+            disableAlias(false)
         }
         // in case the category field is cleared, we need to disable the alias field again
         else{
-             $('#id_alias').prop( "disabled", true );
+             disableAlias(true)
         }
     });
 })
-
 
 // Site field change event
 $(document).ready(function() {
     $('#id_site').change(function(){
+        // If the site changes, category should be reset
+        let catSpanTag = $("#s2id_id_category > a > span")
+        catSpanTag[0].childNodes[0].textContent = "Select category to restrict the list of aliases below"
+        disableAlias(true)
+
         var siteName = $('#id_site').find(":selected").text();
         // If the site is changed to none, alias should be disabled
         if(siteName == "---------"){
-            $('#id_alias').prop( "disabled", true );
+            disableAlias(true)
+            disableCategory(true)
         }
         else{
-            $('#id_alias').prop( "disabled", false );
+            disableCategory(false)
         }
     });
 })
-
-

86f2901b4977da00c4c3b7c9777b4a16077210b1
Michael Collins <michaeljcollinsuk@users.noreply.github.com>
2022-07-05 16:18:19 +0100

    fix: add aliase menu node test back in
    Removes old assert that was testing against the custom category change
    list, as this has been replaced by the Category admin change list view,
    which does not include the Alias menu.

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -1,7 +1,18 @@
+from djangocms_alias.utils import is_versioning_enabled
+
 from .base import BaseAliasPluginTestCase
 
 
 class AliasMenuTestCase(BaseAliasPluginTestCase):
+
+    def test_alias_pages_have_no_menu_nodes(self):
+        alias = self._create_alias()
+        with self.login_user_context(self.superuser):
+            response = self.client.get(alias.get_absolute_url())
+            if is_versioning_enabled():
+                self.assertNotContains(response, '<ul class="nav">')
+            else:
+                self.assertInHTML('<ul class="nav"></ul>', response.content.decode())
 
     def test_pages_keep_their_menu_nodes(self):
         """Tests that AliasDisableMenu modifier does not affect

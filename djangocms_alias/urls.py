08ec77b978af0493d46ae92c84975abf40f6ac45
Fabian Braun <fsbraun@gmx.de>
2023-04-01 11:27:09 +0200

    fix: auto-create static alias content in current language (#165)

--- djangocms_alias/urls.py
+++ djangocms_alias/urls.py
@@ -1,37 +1,37 @@
-from django.urls import re_path
+from django.urls import path
 
 
 from . import constants, views  # isort:skip
 
 
 urlpatterns = [
-    re_path(
-        r'^create-alias/$',
+    path(
+        'create-alias/',
         views.create_alias_view,
         name=constants.CREATE_ALIAS_URL_NAME,
     ),
-    re_path(
-        r'^aliases/(?P<pk>\d+)/usage/$',
+    path(
+        'aliases/<int:pk>/usage/',
         views.alias_usage_view,
         name=constants.USAGE_ALIAS_URL_NAME,
     ),
-    re_path(
-        r'^detach-alias/(?P<plugin_pk>\d+)/$',
+    path(
+        'detach-alias/<int:plugin_pk>/',
         views.detach_alias_plugin_view,
         name=constants.DETACH_ALIAS_PLUGIN_URL_NAME,
     ),
-    re_path(
-        r'^delete-alias/(?P<pk>\d+)/$',
+    path(
+        'delete-alias/<int:pk>/',
         views.delete_alias_view,
         name=constants.DELETE_ALIAS_URL_NAME,
     ),
-    re_path(
-        r'^select2/$',
+    path(
+        'select2/',
         views.AliasSelect2View.as_view(),
         name=constants.SELECT2_ALIAS_URL_NAME,
     ),
-    re_path(
-        r'^category-select2/$',
+    path(
+        'category-select2/',
         views.CategorySelect2View.as_view(),
         name=constants.CATEGORY_SELECT2_URL_NAME,
     ),

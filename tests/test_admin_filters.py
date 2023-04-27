d7b126bc42d15461fe9f59482b234a6c133cf1e1
Fabian Braun <fsbraun@gmx.de>
2023-04-27 18:57:44 +0200

    Remove unused admin filter and tests

--- tests/test_admin_filters.py
+++ tests/test_admin_filters.py
@@ -8,72 +8,12 @@ from cms.utils import get_current_site
 from djangocms_alias.constants import (
     SITE_FILTER_NO_SITE_VALUE,
     SITE_FILTER_URL_PARAM,
-    UNPUBLISHED_FILTER_URL_PARAM,
 )
 from djangocms_alias.filters import CategoryFilter
 from djangocms_alias.models import Alias as AliasModel, AliasContent, Category
 from djangocms_alias.utils import is_versioning_enabled
 
 from .base import BaseAliasPluginTestCase
-
-
-class LanguageFiltersTestCase(BaseAliasPluginTestCase):
-
-    def xxtest_language_filter(self):
-        """
-        When rendering aliascontent manager language filter changing the language
-        should filter the results.
-        """
-        category = Category.objects.create(name='Language Filter Category')
-        alias = AliasModel.objects.create(
-            category=category,
-            position=0,
-        )
-        expected_en_content = AliasContent.objects.create(
-            alias=alias,
-            name="EN Alias Content",
-            language="en",
-        )
-        expected_de_content = AliasContent.objects.create(
-            alias=alias,
-            name="DE Alias Content",
-            language="de",
-        )
-        # If versioning is enabled be sure to create a version
-        if is_versioning_enabled():
-            from djangocms_versioning.models import Version
-
-            Version.objects.create(content=expected_en_content, created_by=self.superuser)
-            Version.objects.create(content=expected_de_content, created_by=self.superuser)
-
-        base_url = self.get_admin_url(AliasModel, "changelist")
-
-        with self.login_user_context(self.superuser):
-            # en is the default language configured for the site
-            response_default = self.client.get(base_url)
-            # en should have a result
-            response_en = self.client.get(base_url + "?language=en")
-            # de should have a result
-            response_de = self.client.get(base_url + "?language=de")
-            # fr should have no result and be empty because nothing was created
-            response_fr = self.client.get(base_url + "?language=fr")
-
-        self.assertEqual(
-            set(response_default.context["cl"].queryset),
-            {expected_en_content}
-        )
-        self.assertEqual(
-            set(response_en.context["cl"].queryset),
-            {expected_en_content}
-        )
-        self.assertEqual(
-            set(response_de.context["cl"].queryset),
-            {expected_de_content}
-        )
-        self.assertEqual(
-            set(response_fr.context["cl"].queryset),
-            set()
-        )
 
 
 class SiteFiltersTestCase(BaseAliasPluginTestCase):
@@ -171,54 +111,6 @@ class SiteFiltersTestCase(BaseAliasPluginTestCase):
             set(response_empty_site.context["cl"].queryset),
             set()
         )
-
-
-@skipUnless(is_versioning_enabled(), 'Test only relevant for versioning')
-class UnpublishedFiltersTestCase(BaseAliasPluginTestCase):
-
-    def xxtest_unpublished_filter(self):
-        """
-        When rendering alias change list unpublished filter changing the show/hide unpublished version option
-        should filter the results.
-        """
-        from djangocms_versioning.constants import UNPUBLISHED
-        from djangocms_versioning.models import Version
-
-        category = Category.objects.create(name='Alias Filter Category')
-        alias = AliasModel.objects.create(
-            category=category,
-            position=0
-        )
-        unpublished_alias = AliasModel.objects.create(
-            category=category,
-            position=0)
-        expected_en_content = AliasContent.objects.create(
-            alias=alias,
-            name="EN Alias Content",
-            language="en"
-        )
-        Version.objects.create(content=expected_en_content, created_by=self.superuser)
-        expected_unpublished = AliasContent.objects.create(
-            alias=unpublished_alias,
-            name="EN Alias Content unpublished",
-            language="en",
-        )
-        Version.objects.create(content=expected_unpublished, created_by=self.superuser, state=UNPUBLISHED)
-        base_url = self.get_admin_url(AliasModel, "changelist")
-
-        with self.login_user_context(self.get_superuser()):
-            # en is the default language configured for the site
-            response_default = self.client.get(base_url)
-            # filter by unpublished hide
-            qs_default = response_default.context["cl"].queryset
-            response_unpublished = self.client.get(f"{base_url}?{UNPUBLISHED_FILTER_URL_PARAM}=1")
-            qs_unpublished = response_unpublished.context["cl"].queryset
-            # filter by unpublished show
-
-        # show all alias contents  excluding unpublished versions
-        self.assertEqual(set(qs_default), {expected_en_content})
-        # show all aliase contents including unpublished versions
-        self.assertEqual(set(qs_unpublished), {expected_unpublished})
 
 
 class CatergoryFiltersTestCase(BaseAliasPluginTestCase):

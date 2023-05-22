a0cfb4223359b92698dc9421cfc3e2e98ab40cc6
Fabian Braun <fsbraun@gmx.de>
2023-05-22 11:46:05 +0200

    fix flake8

--- djangocms_alias/forms.py
+++ djangocms_alias/forms.py
@@ -245,6 +245,7 @@ class Select2Mixin:
             'djangocms_alias/js/alias_plugin.js',
         )
 
+
 class CategorySelectWidget(Select2Mixin, forms.TextInput):
     def get_url(self):
         return admin_reverse(CATEGORY_SELECT2_URL_NAME)
@@ -306,7 +307,6 @@ class AliasPluginForm(forms.ModelForm):
         # If the form is changing an existing Alias
         # Be sure to show the values for an Alias
         if self.instance and self.instance.pk:
-            # self.fields['site'].initial = self.instance.alias.site
             self.fields['category'].initial = self.instance.alias.category
         # Otherwise this is creation
         # Set the site to the current site by default

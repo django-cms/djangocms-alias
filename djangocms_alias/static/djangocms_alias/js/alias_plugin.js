e9ae5634b50d903b349e427b82b383e1c759afe3
Fabian Braun <fsbraun@gmx.de>
2023-05-22 17:43:06 +0200

    Update some comments

--- djangocms_alias/static/djangocms_alias/js/alias_plugin.js
+++ djangocms_alias/static/djangocms_alias/js/alias_plugin.js
@@ -1,11 +1,10 @@
-/* If the category field is changed the alias field's value is reset.
- */
+/* If the category field is changed the alias field's value is reset. */
 
 $(document).ready(function() {
     // Category field change event
     $('#id_category').change(function(){
-        let categoryValue = $('#id_category').select2('val');
-        // if category is set, remove the value from alias (which might be of another category)
+        const categoryValue = $('#id_category').select2('val');
+        // if category is set, remove the value from alias (which might be of a different category)
         if (categoryValue) {
             $('#id_alias').select2('val', '');
         }

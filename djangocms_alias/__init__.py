5ffaa2ed3b03ce7322eff35cbb1b30ef46f31832
Fabian Braun <fsbraun@gmx.de>
2025-05-15 22:38:06 +0200

    chore: Prepare release 2.0.3 (#269)
    * Update __init__.py
    
    * Update CHANGELOG.rst
    
    * Update pyproject.toml

--- djangocms_alias/__init__.py
+++ djangocms_alias/__init__.py
@@ -3,4 +3,4 @@ An alias is a collection of plugins that is managed centrally.
 A reference can be added to any placeholder using the Alias plugin.
 """
 
-__version__ = "2.0.2"
+__version__ = "2.0.3"

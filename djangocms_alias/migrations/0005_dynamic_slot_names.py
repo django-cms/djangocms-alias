fa61e0cca4011f7758ae70ad5e3aca22cbdc08fa
Fabian Braun <fsbraun@gmx.de>
2025-08-28 17:20:44 +0200

    fix: default_slot_name access

--- djangocms_alias/migrations/0005_dynamic_slot_names.py
+++ djangocms_alias/migrations/0005_dynamic_slot_names.py
@@ -2,9 +2,13 @@ from django.db import migrations, transaction
 
 
 def migrate_slots(apps, schema_editor, forward=True):
+    from djangocms_alias.models import AliasContent as AliasContentModelClass
+
     AliasContent = apps.get_model("djangocms_alias", "AliasContent")
     ContentType = apps.get_model("contenttypes", "ContentType")
     Placeholder = apps.get_model("cms", "Placeholder")
+
+    default_slot_name = AliasContentModelClass.placeholder_slotname
 
     db_alias = schema_editor.connection.alias
     content_type = ContentType.objects.filter(app_label="djangocms_alias", model="aliascontent").first()
@@ -19,13 +23,13 @@ def migrate_slots(apps, schema_editor, forward=True):
             if len(slots) == 1:
                 placeholder = slots[0]
                 # Ensure the placeholder exists with the correct slot name
-                if forward and placeholder.slot == alias_content.placeholder_slotname:
+                if forward and placeholder.slot == default_slot_name:
                     # If migrating forward, we use the static code or the placeholder slot name
-                    placeholder.slot = alias_content.alias.static_code or alias_content.placeholder_slotname
+                    placeholder.slot = alias_content.alias.static_code or default_slot_name
                     placeholder.save()
-                elif placeholder.slot != alias_content.placeholder_slotname:
+                elif placeholder.slot != default_slot_name:
                     # If migrating backward, we revert to the original placeholder slot name
-                    placeholder.slot = alias_content.placeholder_slotname
+                    placeholder.slot = default_slot_name
                     placeholder.save()
             elif len(slots) > 1:
                 print(

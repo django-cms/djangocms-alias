e8e836efbc65c3989f3768847158b28ada4dc443
Fabian Braun <fsbraun@gmx.de>
2025-08-28 16:21:19 +0200

    fix: Avoid creating content type

--- djangocms_alias/migrations/0005_dynamic_slot_names.py
+++ djangocms_alias/migrations/0005_dynamic_slot_names.py
@@ -7,7 +7,9 @@ def migrate_slots(apps, schema_editor, forward=True):
     Placeholder = apps.get_model("cms", "Placeholder")
 
     db_alias = schema_editor.connection.alias
-    content_type, _ = ContentType.objects.get_or_create(app_label="djangocms_alias", model="aliascontent")
+    content_type = ContentType.objects.filter(app_label="djangocms_alias", model="aliascontent").first()
+    if content_type is None:
+        return
     placeholder_qs = Placeholder.objects.using(db_alias).filter(content_type=content_type)
     qs = AliasContent._default_manager.using(db_alias).prefetch_related("alias").exclude(alias__static_code="")
 

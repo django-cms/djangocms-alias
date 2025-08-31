72303eb877e01c0d2c8288f4d294dbc4b9c5df2b
Fabian Braun <fsbraun@gmx.de>
2025-08-31 19:01:17 +0200

    fix: Migration 0005 used manager not available in migration (#288)
    * fix: Migration 0005 used manager not available in migration
    
    * fix: get_or_create
    
    * fix: Avoid creating content type
    
    * fix: default_slot_name access

--- djangocms_alias/migrations/0005_dynamic_slot_names.py
+++ djangocms_alias/migrations/0005_dynamic_slot_names.py
@@ -2,24 +2,34 @@ from django.db import migrations, transaction
 
 
 def migrate_slots(apps, schema_editor, forward=True):
+    from djangocms_alias.models import AliasContent as AliasContentModelClass
+
     AliasContent = apps.get_model("djangocms_alias", "AliasContent")
+    ContentType = apps.get_model("contenttypes", "ContentType")
+    Placeholder = apps.get_model("cms", "Placeholder")
+
+    default_slot_name = AliasContentModelClass.placeholder_slotname
 
     db_alias = schema_editor.connection.alias
-    qs = AliasContent.objects.using(db_alias).prefetch_related("alias").exclude(alias__static_code="")
+    content_type = ContentType.objects.filter(app_label="djangocms_alias", model="aliascontent").first()
+    if content_type is None:
+        return
+    placeholder_qs = Placeholder.objects.using(db_alias).filter(content_type=content_type)
+    qs = AliasContent._default_manager.using(db_alias).prefetch_related("alias").exclude(alias__static_code="")
 
     with transaction.atomic(using=db_alias):
         for alias_content in qs:
-            slots = list(alias_content.placeholders.all())
+            slots = list(placeholder_qs.filter(object_id=alias_content.pk))
             if len(slots) == 1:
                 placeholder = slots[0]
                 # Ensure the placeholder exists with the correct slot name
-                if forward and placeholder.slot == alias_content.placeholder_slotname:
+                if forward and placeholder.slot == default_slot_name:
                     # If migrating forward, we use the static code or the placeholder slot name
-                    placeholder.slot = alias_content.alias.static_code or alias_content.placeholder_slotname
+                    placeholder.slot = alias_content.alias.static_code or default_slot_name
                     placeholder.save()
-                elif placeholder.slot != alias_content.placeholder_slotname:
+                elif placeholder.slot != default_slot_name:
                     # If migrating backward, we revert to the original placeholder slot name
-                    placeholder.slot = alias_content.placeholder_slotname
+                    placeholder.slot = default_slot_name
                     placeholder.save()
             elif len(slots) > 1:
                 print(

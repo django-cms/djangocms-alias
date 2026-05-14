38522a9b41908e43fa931cbb3a1d6da275f862d4
Fabian Braun <fsbraun@gmx.de>
2026-05-14 07:46:26 +0200

    fix: Remove outdated django CMS select2 dependency (#362)

--- djangocms_alias/static/djangocms_alias/js/alias_plugin.js
+++ djangocms_alias/static/djangocms_alias/js/alias_plugin.js
@@ -1,12 +1,12 @@
-/* If the category field is changed the alias field's value is reset. */
+'use strict';
+{
+    const $ = django.jQuery;
 
-$(document).ready(function() {
-    // Category field change event
-    $('#id_category').change(function(){
-        const categoryValue = $('#id_category').select2('val');
-        // if category is set, remove the value from alias (which might be of a different category)
-        if (categoryValue) {
-            $('#id_alias').select2('val', '');
-        }
+    $(function() {
+        $('#id_category').on('change', function() {
+            if ($(this).val()) {
+                $('#id_alias').val(null).trigger('change');
+            }
+        });
     });
-});
+}

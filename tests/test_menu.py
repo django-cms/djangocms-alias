22452593e3c9c01d1c57a884c69d0ed5509070f7
Fabian Braun <fsbraun@gmx.de>
2026-05-14 10:44:32 +0200

    chore: Update test deps (#365)
    * chore: Update test deps
    
    * fix test

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -1,19 +1,14 @@
 from cms.toolbar.utils import get_object_edit_url
 
-from djangocms_alias.utils import is_versioning_enabled
-
 from .base import BaseAliasPluginTestCase
 
 
 class AliasMenuTestCase(BaseAliasPluginTestCase):
-    def test_alias_pages_have_no_menu_nodes(self):
+    def test_alias_pages_have_menu_nodes(self):
         alias = self._create_alias()
         with self.login_user_context(self.superuser):
             response = self.client.get(get_object_edit_url(alias.get_content()), follow=True)
-            if is_versioning_enabled() and False:
-                self.assertNotContains(response, '<ul class="nav">')
-            else:
-                self.assertInHTML('<ul class="nav"></ul>', response.content.decode())
+            self.assertInHTML('<ul class="nav"></ul>', response.content.decode())
 
     def test_pages_keep_their_menu_nodes(self):
         """Tests that AliasDisableMenu modifier does not affect

9b4a06e5d2eea92098efd11a2d7d10924eb57c72
Robert Stein <robert@blueshoe.io>
2026-08-07 10:20:46 +0200

    fix: alias modifier

--- djangocms_alias/cms_menus.py
+++ djangocms_alias/cms_menus.py
@@ -9,6 +9,8 @@ class AliasDisableMenu(Modifier):
     """Disable menu rendering on alias pages"""
 
     def modify(self, request, nodes, namespace, root_id, post_cut, breadcrumb):
+        if not hasattr(request, "toolbar") or not request.toolbar:
+            return nodes
         if request.toolbar.app_name == PLUGIN_URL_NAME_PREFIX or isinstance(request.toolbar.obj, AliasContent):
             return []
         return nodes
